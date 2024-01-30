3f6171e313f676961444e88e93eff344e68d2914
Julien <julien.thoumy@gmail.com>
2024-01-30 15:36:34 +0100

    config projet

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import './assets/main.css'
-
+import 'animate.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPeristedstate from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()pinia.use(piniaPluginPeristedstate)
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
